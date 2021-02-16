a307d01b344a9e43a6f789e2ce218154ccc2b9ae
koral <orszulak.k@o2.pl>
2021-02-16 23:42:19 +0100

    messages, disabled chat

--- src/main/java/koral/premiumlogin/PremiumLogin.java
+++ src/main/java/koral/premiumlogin/PremiumLogin.java
@@ -1,5 +1,11 @@
 package koral.premiumlogin;
 
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.events.ListenerPriority;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketEvent;
 import fr.xephi.authme.api.v3.AuthMeApi;
 import koral.premiumlogin.database.DatabaseConnection;
 import koral.premiumlogin.database.Table;
@@ -18,6 +24,24 @@ public final class PremiumLogin extends JavaPlugin {
 
         getServer().getPluginManager().registerEvents(new PlayerJoin(), this);
         // Plugin startup logic
+
+    }
+    private ProtocolManager protocolManager;
+    @Override
+    public void onLoad() {
+        protocolManager = ProtocolLibrary.getProtocolManager();
+
+        protocolManager.addPacketListener(
+                new PacketAdapter(this, ListenerPriority.NORMAL,
+                        PacketType.Play.Server.CHAT) {
+                    @Override
+                    public void onPacketSending(PacketEvent event) {
+                        if (event.getPacketType() == PacketType.Play.Server.CHAT) {
+                            if(PlayerJoin.disabledChat.contains(event.getPlayer().getName()))
+                              event.setCancelled(true);
+                        }
+                    }
+                });
 
     }
 
--- src/main/java/koral/premiumlogin/listeners/PlayerJoin.java
+++ src/main/java/koral/premiumlogin/listeners/PlayerJoin.java
@@ -4,23 +4,39 @@ import fr.xephi.authme.api.v3.AuthMeApi;
 import koral.premiumlogin.PremiumLogin;
 import koral.premiumlogin.database.Statements;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
+import java.util.HashSet;
+import java.util.Set;
+
 
 public class PlayerJoin implements Listener {
+    final String prefix = "§7§l[§4§lJBWM§7§l]";
+    public static Set<String> disabledChat = new HashSet<>();
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent e){
         Bukkit.getScheduler().runTaskAsynchronously(PremiumLogin.getPlugin(PremiumLogin.class), () -> Statements.createPlayerQuery(e.getPlayer()));
 
         Bukkit.getScheduler().runTaskAsynchronously(PremiumLogin.getPlugin(PremiumLogin.class), () -> {
            if(Statements.isPremium(e.getPlayer())){
+               disabledChat.add(e.getPlayer().getName());
               AuthMeApi.getInstance().forceUnregister(e.getPlayer());
-              Bukkit.getScheduler().runTaskLaterAsynchronously(PremiumLogin.getPlugin(PremiumLogin.class), () -> AuthMeApi.getInstance().forceRegister(e.getPlayer(), "1234567wdfsd", true), 10);
+              Bukkit.getScheduler().runTaskLaterAsynchronously(PremiumLogin.getPlugin(PremiumLogin.class), () -> {
+                  AuthMeApi.getInstance().forceRegister(e.getPlayer(), "1234567wdfsd", false);
+                  }, 4);
+              Bukkit.getScheduler().runTaskLaterAsynchronously(PremiumLogin.getPlugin(PremiumLogin.class), () -> AuthMeApi.getInstance().forceLogin(e.getPlayer()), 6);
+               Bukkit.getScheduler().runTaskLater(PremiumLogin.getPlugin(PremiumLogin.class), ()-> {
+                   disabledChat.remove(e.getPlayer().getName());
+                   e.getPlayer().sendMessage(prefix + "§a Zalogowano z konta premium.");
+                   e.getPlayer().sendMessage(prefix + "§c Logowanie jest w fazie BETA, jeżeli masz problem z logowaniem premium zrób relog, zgłoś problem administratorowi.");
+               }, 18);
            }
         });
+
     }
 
     @EventHandler
@@ -30,6 +46,8 @@ public class PlayerJoin implements Listener {
         });
 
     }
+
+
 
 
 
