b33c7b2a1430227dc635f2c2f0bba959bd151f7a
koral <orszulak.k@o2.pl>
2021-05-27 18:01:02 +0200

    fix wrong timed asynchronous functions

--- src/main/java/koral/premiumlogin/listeners/PlayerJoin.java
+++ src/main/java/koral/premiumlogin/listeners/PlayerJoin.java
@@ -1,18 +1,19 @@
 package koral.premiumlogin.listeners;
 
+
 import fr.xephi.authme.api.v3.AuthMeApi;
 import koral.premiumlogin.PremiumLogin;
 import koral.premiumlogin.database.Statements;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
-
 import java.util.HashSet;
+import java.util.Random;
 import java.util.Set;
-
+import java.util.stream.Collectors;
 
 public class PlayerJoin implements Listener {
     final String prefix = "§7§l[§4§lJBWM§7§l]";
@@ -22,22 +23,42 @@ public class PlayerJoin implements Listener {
         Bukkit.getScheduler().runTaskAsynchronously(PremiumLogin.getPlugin(PremiumLogin.class), () -> Statements.createPlayerQuery(e.getPlayer()));
 
         Bukkit.getScheduler().runTaskAsynchronously(PremiumLogin.getPlugin(PremiumLogin.class), () -> {
-           if(Statements.isPremium(e.getPlayer())){
-               disabledChat.add(e.getPlayer().getName());
-              AuthMeApi.getInstance().forceUnregister(e.getPlayer());
-              Bukkit.getScheduler().runTaskLaterAsynchronously(PremiumLogin.getPlugin(PremiumLogin.class), () -> {
-                  AuthMeApi.getInstance().forceRegister(e.getPlayer(), "1234567wdfsd", false);
-                  }, 4);
-              Bukkit.getScheduler().runTaskLaterAsynchronously(PremiumLogin.getPlugin(PremiumLogin.class), () -> AuthMeApi.getInstance().forceLogin(e.getPlayer()), 6);
-               Bukkit.getScheduler().runTaskLater(PremiumLogin.getPlugin(PremiumLogin.class), ()-> {
-                   disabledChat.remove(e.getPlayer().getName());
-                   e.getPlayer().sendMessage(prefix + "§a Zalogowano z konta premium.");
-                   e.getPlayer().sendMessage(prefix + "§c Logowanie jest w fazie BETA, jeżeli masz problem z logowaniem premium zrób relog, zgłoś problem administratorowi.");
-               }, 18);
-           }
+            if(Statements.isPremium(e.getPlayer())){
+
+
+                loginPremiumPlayer(e.getPlayer(), 6);
+
+                Bukkit.getScheduler().runTaskLater(PremiumLogin.getPlugin(PremiumLogin.class), ()-> {
+                    if(AuthMeApi.getInstance().isAuthenticated(e.getPlayer())) {
+                        enableChat(e.getPlayer());
+                    }
+                    else{
+                        loginPremiumPlayer(e.getPlayer(), 12);
+                        Bukkit.getScheduler().runTaskLater(PremiumLogin.getPlugin(PremiumLogin.class), () -> {
+                            if(AuthMeApi.getInstance().isAuthenticated(e.getPlayer()))
+                                enableChat(e.getPlayer());
+                            else e.getPlayer().kickPlayer("Błąd logowania premium, spróbuj ponownie!");
+
+                        }, 30);
+                    }
+                }, 18);
+            }
         });
 
     }
+    //6, 12
+    public void loginPremiumPlayer(Player player, int registerDelay){
+        String password = new Random().ints(10, 33, 122).mapToObj(i -> String.valueOf((char)i)).collect(Collectors.joining());
+        disabledChat.add(player.getName());
+        AuthMeApi.getInstance().forceUnregister(player);
+        Bukkit.getScheduler().runTaskLater(PremiumLogin.getPlugin(PremiumLogin.class), () -> AuthMeApi.getInstance().forceRegister(player, password, true), registerDelay);
+    }
+
+    public void enableChat(Player player){
+        disabledChat.remove(player.getName());
+        player.sendMessage(prefix + "§a Zalogowano z konta premium.");
+    }
+
 
     @EventHandler
     public void onPlayerQuit(PlayerQuitEvent e){
